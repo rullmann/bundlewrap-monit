2b53ff602b9ff33edf48033c3c5d473268aafd3e
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:14 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -5,33 +5,33 @@ pkg_dnf = {
 svc_systemd = {
     'monit': {
         'needs': [
-            "pkg_dnf:monit",
-            "file:/etc/monitrc",
+            'pkg_dnf:monit',
+            'file:/etc/monitrc',
         ],
     },
 }
 
 files = {
     '/etc/monitrc': {
-        'source': "monitrc",
-        'mode': "0700",
-        'content_type': "mako",
+        'source': 'monitrc',
+        'mode': '0700',
+        'content_type': 'mako',
         'needs': [
-            "pkg_dnf:monit",
+            'pkg_dnf:monit',
         ],
         'triggers': [
-            "svc_systemd:monit:restart",
+            'svc_systemd:monit:restart',
         ],
     },
     '/etc/monit.d/system': {
-        'source': "system",
-        'mode': "0700",
-        'content_type': "mako",
+        'source': 'system',
+        'mode': '0700',
+        'content_type': 'mako',
         'needs': [
-            "pkg_dnf:monit",
+            'pkg_dnf:monit',
         ],
         'triggers': [
-            "svc_systemd:monit:restart",
+            'svc_systemd:monit:restart',
         ],
     },
 }
@@ -39,9 +39,9 @@ files = {
 if node.metadata.get('monit', {}).get('disable_notification_time', False):
     files['/etc/cron.d/monit'] = {
         'content_type': 'mako',
-        'source': "crond",
-        'mode': "0644",
+        'source': 'crond',
+        'mode': '0644',
         'needs': [
-            "pkg_dnf:monit",
+            'pkg_dnf:monit',
         ]
     }
